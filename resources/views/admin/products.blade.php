892f6d43c9bae410a9cd23fdf3e0a668787a783e
coffeeSR <shaktirajbhandari@gmail.com>
2024-12-27 16:03:33 +0545

    Fifth commit

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -84,12 +84,14 @@
                                             <i class="icon-eye"></i>
                                         </div>
                                     </a>
-                                    <a href="#">
+                                    <a href="{{ route('admin.product.edit', ['id'=>$product->id]) }}">
                                         <div class="item edit">
                                             <i class="icon-edit-3"></i>
                                         </div>
                                     </a>
-                                    <form action="#" method="POST">
+                                    <form action="{{ route('admin.product.delete', ['id'=>$product->id]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
                                         <div class="item text-danger delete">
                                             <i class="icon-trash-2"></i>
                                         </div>
@@ -111,3 +113,26 @@
     </div>
 </div>
 @endsection
+
+@push('scripts')
+    <script>
+        $(function()
+        {
+            $('.delete').on('click', function(e){
+                e.preventDefault();
+                var form = $(this).closest('form');
+                swal({
+                    title: "Are you sure?",
+                    text: "Once deleted, you will not be able to recover this data",
+                    type: "warning",
+                    buttons: ['No','Yes'],
+                    confirmButtonColor: "dc3545",
+                }).then(function(result){
+                    if(result){
+                        form.submit();
+                    }
+                })
+            });
+        });
+    </script>
+@endpush
